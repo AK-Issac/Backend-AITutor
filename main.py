f5920090d00816ec2c01f7f32ae4505421996b15
dvp536 <missdegat@hotmail.com>
2025-02-20 15:55:47 -0500

    Add context to AI to Lecture section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from lib.chat import send
+from lib.chat import send, answerquestion, askquestion
 from lib.speech_recognition import transcribe
 
 app = Flask(__name__)
@@ -8,7 +8,9 @@ CORS(app)
 
 # API Endpoint pour analyser la reformulation
 app.add_url_rule('/api/chat', 'send', send, methods=['POST'])
-app.add_url_rule('/api/transcribe', 'transcribe', transcribe, methods=['POST'])
+app.add_url_rule('/api/lecture/transcribe', 'transcribe', transcribe, methods=['POST'])
+app.add_url_rule('/api/lecture/answerquestion', 'answerquestion', answerquestion, methods=['POST'])
+app.add_url_rule('/api/lecture/askquestion', 'askquestion', askquestion, methods=['POST'])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
